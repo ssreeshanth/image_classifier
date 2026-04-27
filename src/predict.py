dd2164f7ebd26e9b35d5bf0bebb08a20937b831e
ssreeshanth <sreeshanthrao.2@gmail.com>
2026-04-27 11:04:04 +0530

    Refactor prediction function and add decoding
    Renamed predict function to predict_image and added decoding of predictions.

--- src/predict.py
+++ src/predict.py
@@ -1,13 +1,16 @@
 import numpy as np
 from tensorflow.keras.preprocessing import image
+from src.utils import decode_prediction
 
 def preprocess(img_path, target_size=(224, 224)):
     img = image.load_img(img_path, target_size=target_size)
     img_array = image.img_to_array(img)
+    img_array = img_array / 255.0
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
 
-def predict(model, img_path):
+def predict_image(model, img_path):
     img = preprocess(img_path)
-    preds = model.predict(img)
-    return preds
+    pred = model.predict(img)
+    label = decode_prediction(pred)
+    return label, pred
